b695e1a90784a07601b95a99c728ef3c76433822
soufg <soufg@DESKTOP-0EI3HKD>
2023-10-04 00:13:33 +0100

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly AppDbContext appDbContext;
+        public EmployeesController(AppDbContext appDbContext)
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployees addEmployeesRequest)
+        {
+            var employee = new Employees()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeesRequest.Name,
+                Age = addEmployeesRequest.Age
+            };
+            await appDbContext.Employees.AddAsync(employee);
+            await appDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+
+        }
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+namespace WebApplication1.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employees> Employees { get; set; }
+
+    }
+}
